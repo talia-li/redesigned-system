478409a5269a31bd865f12a10e1e093405634f24
talia-li <60248153+talia-li@users.noreply.github.com>
2021-10-28 22:52:48 -0400

    11

---
+++ Java-script/script.js
@@ -0,0 +1,19 @@
+function sayHello(){
+    console.log("Hi");
+    }
+    
+    sayHello();
+
+    let age = 25;
+    let height = 5;
+
+    console.log(age);
+    console.log(height);
+
+    let noun = "bird";
+    let verb = "fly";
+    let adjective = "perfectly";
+    let sentence =  `My ${noun} ${verb} ${adjective} `;
+
+    console.log(sentence);
+   
