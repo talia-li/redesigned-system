9335ee5452d9bc2ae6cdee85e1f87c366a1982df
talia-li <60248153+talia-li@users.noreply.github.com>
2021-11-09 20:15:41 -0500

    java

---
+++ Java-script/script-02.js
@@ -0,0 +1,4 @@
+let age = 12;
+if(age<8) {console.log("check out the merry-go-round. You'll love it!")
+}else if (age>8 && age<65) {console.log("check out the roller coaster")
+}else {console.log("check out the lazy river!")}
